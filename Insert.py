4e9d44c74c979f6587bd31c0f5ac4228aca7b002
Felipe Cunha <54443777+Study98@users.noreply.github.com>
2022-02-24 01:29:29 +0000

    lista para inserir os dados

--- Insert.py
+++ Insert.py
@@ -1,5 +1,20 @@
+dads = []
+
 class Dados:
   def __init__(self, nick, nome, cidade):
     self.nick = nick
     self.nome = nome
     self.cidade = cidade
+
+def criar():
+  nick = input("Informe o seu Apelido: ")
+  nome = input("Informe o seu Nome Completo: ") 
+  cidade = input("Informe sua Cidade: ")
+  d= Dados(nick.upper(),nome.upper(),cidade.upper())
+  
+  dads.append(d)
+
+def listar():
+  for d in dads:
+    print("------------------------------------")
+    print("Apelido: {}\nNome Completo: {}\nCidade: {}".format(d.nick,d.nome,d.cidade))
--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 from Insert import *
 
-nick = input("Informe o seu Apelido: ")
-nome = input("Informe o seu Nome Completo: ") 
-cidade = input("Informe sua Cidade: ")
-
-d= Dados(nick.upper(),nome.upper(),cidade.upper())
-
-print("------------------------------------")
-print("Apelido: {}\nNome Completo: {}\nCidade: {}".format(d.nick,d.nome,d.cidade))
+criar()
+listar()
