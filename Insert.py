e945716b9f70117d5bc001209045f417585a7067
Felipe Cunha <54443777+Study98@users.noreply.github.com>
2022-02-24 01:53:50 +0000

    Menu com funções de criar e listar

--- Insert.py
+++ Insert.py
@@ -1,3 +1,4 @@
+import os
 dads = []
 
 class Dados:
@@ -7,14 +8,19 @@ class Dados:
     self.cidade = cidade
 
 def criar():
+  print("----------------Inserindo--------------------")
   nick = input("Informe o seu Apelido: ")
   nome = input("Informe o seu Nome Completo: ") 
   cidade = input("Informe sua Cidade: ")
   d= Dados(nick.upper(),nome.upper(),cidade.upper())
-  
+  limpar()
   dads.append(d)
 
 def listar():
+  print("----------------Lista Completa----------------")
   for d in dads:
     print("------------------------------------")
     print("Apelido: {}\nNome Completo: {}\nCidade: {}".format(d.nick,d.nome,d.cidade))
+
+def limpar():
+  os.system("clear")
--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from Insert import *
 
-criar()
-listar()
+run = True
+
+while run:
+  print("-------------Opções---------------")
+  a=int(input("1 - Inserir Dados\n2 - Listar Dados\n3 - Finalizar\nInforme Sua Opção: "))
+  if a==1:
+    limpar()
+    criar()
+    print("Inserido com sucesso")
+  elif a==2:
+    limpar()
+    listar()
+  elif a==3:
+    limpar()
+    run = False
+    print("Programa Finalizado! Para uma nova execução, apertar o botão verde ;)")
